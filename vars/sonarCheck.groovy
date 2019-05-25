9aa39203dfceadaa02bff346731e301696e7b5bf
jprakash197 <50922701+jprakash197@users.noreply.github.com>
2019-05-26 00:55:32 +0530

    Create sonarCheck.groovy

---
+++ vars/sonarCheck.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+def call(String mvnHome, String sonarQubeEnv){
+   withEnv(["MVN_HOME=${mvnHome}"]){
+           withSonarQubeEnv(${sonarQubeEnv}) {
+               sh '${MVN_HOME}/bin/mvn sonar:sonar'
+             }
+        }
+           timeout(time: 1, unit: 'HOURS') {
+                def qg = waitForQualityGate() 
+                if (qg.status != 'OK') { 
+                  error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                    }
+             }
+} 
