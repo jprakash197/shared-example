438434a7393b3e578502b81c1a8dde2116d0ed4c
jprakash197 <50922701+jprakash197@users.noreply.github.com>
2019-05-26 01:18:05 +0530

    Update dockerImageBuild.groovy

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -2,5 +2,5 @@
 #!/usr/bin/env groovy
 def call(String imageName){
  // sh 'sudo docker build -t jyoti_img .' 
-  sh 'sudo docker build -t "$(imageName)" .' 
+    sh 'sudo docker build -t "${imageName}" .' 
  }
