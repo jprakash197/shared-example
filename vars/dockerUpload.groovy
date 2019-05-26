dc1418ad0ce62c1665cb9a7889e1248fde45aba9
jprakash197 <50922701+jprakash197@users.noreply.github.com>
2019-05-26 20:16:54 +0530

    Update dockerUpload.groovy

--- vars/dockerUpload.groovy
+++ vars/dockerUpload.groovy
@@ -2,5 +2,5 @@
 def call(String userNameDocker, String passwordDocker, String imageName ){
          sh "sudo docker login -u ${userNameDocker} -p ${passwordDocker}"
          sh "sudo docker tag ${imageName} jprakash197/${imageName}"
-         sh "sudo docker push jprakash197/${imageName}:tagname"
+         sh "sudo docker push jprakash197/${imageName}"
 }
