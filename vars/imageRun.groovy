87aeaf877520d33b2b2c3639873a8154210a1a6d
jprakash197 <50922701+jprakash197@users.noreply.github.com>
2019-05-27 09:29:35 +0530

    Update imageRun.groovy

--- vars/imageRun.groovy
+++ vars/imageRun.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 def call(String firstContainer, String imageName, String firstHostPort, String secondContainer, String secondHostPort, String containerPort){
-        sh 'sudo docker rm -f ${firstContainer} ${secondContainer}'
-        sh 'sudo docker run -d -p ${firstHostPort}:${containerPort} --name ${firstContainer} ${imageName}'
-        sh 'sudo docker run -d -p ${secondHostPort}:${containerPort} --name ${secondContainer} ${imageName}'
+        sh "sudo docker rm -f ${firstContainer} ${secondContainer}"
+        sh "sudo docker run -d -p ${firstHostPort}:${containerPort} --name ${firstContainer} ${imageName}"
+        sh "sudo docker run -d -p ${secondHostPort}:${containerPort} --name ${secondContainer} ${imageName}"
  }
 
        
